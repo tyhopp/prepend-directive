3e416c0a7e7ae46cc553da20cd74d09e1bae30f0
Ty Hopp <tyhopp@users.noreply.github.com>
2022-10-24 21:14:08 -0700

    Add CJS interface (#4)

--- README.md
+++ README.md
@@ -4,10 +4,22 @@ Prepend a directive to the top of a file.
 
 Useful as a [post build script](https://docs.npmjs.com/cli/v8/using-npm/scripts#pre--post-scripts) if your build tool (e.g. [microbundle](https://github.com/developit/microbundle)) strips comments and does not allow you to configure the behavior conditionally.
 
-## Usage
+## CLI interface
 
 ```shell
-npx prepend-directive@latest --files=file-a.js,file-b.js --directive=\"use strict\"
+npx prepend-directive@latest --directive=\"use strict\" --files=file-a.js,file-b.js
+```
+
+## Node CJS interface
+
+```js
+const prependDirective = require(`prepend-directive`);
+
+prependDirective({
+  directive: `use strict`,
+  files: [`file-a.js`, `file-b.js`],
+  cwd: __dirname, // Optional
+});
 ```
 
 ### Before
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const args = process.argv.slice(2);
 
 let directive;
 let files = [];
+let cwd = process.cwd();
 
 for (const arg of args) {
   const [key, value] = arg.split(`=`);
@@ -18,28 +19,37 @@ for (const arg of args) {
     case `--files`:
       files = value.split(`,`);
       break;
+    case `--cwd`:
+      cwd = value;
+      break;
   }
 }
 
-for (const file of files) {
-  try {
-    const filePath = path.resolve(process.cwd(), file);
-    const fileContent = fs.readFileSync(filePath).toString();
+function prependDirective({ directive, files, cwd = process.cwd() }) {
+  for (const file of files) {
+    try {
+      const filePath = path.resolve(cwd, file);
+      const fileContent = fs.readFileSync(filePath).toString();
 
-    fs.writeFileSync(filePath, `"${directive}"\n${fileContent}`);
+      fs.writeFileSync(filePath, `"${directive}"\n${fileContent}`);
 
-    console.info(
-      `\x1b[32m`,
-      `Prepended directive "${directive}" to file ${file}`,
-      `\x1b[0m`
-    );
-  } catch (error) {
-    console.error(
-      `\x1b[31m`,
-      `Failed to prepend directive "${directive}" to file "${file}"\n`,
-      `\x1b[0m`,
-      error
-    );
-    break;
+      console.info(
+        `\x1b[32m`,
+        `Prepended directive "${directive}" to file ${file}`,
+        `\x1b[0m`
+      );
+    } catch (error) {
+      console.error(
+        `\x1b[31m`,
+        `Failed to prepend directive "${directive}" to file "${file}"\n`,
+        `\x1b[0m`,
+        error
+      );
+      break;
+    }
   }
 }
+
+prependDirective({ directive, files, cwd });
+
+module.exports = prependDirective;
